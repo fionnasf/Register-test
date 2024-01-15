00187abb4ffe4789bea664b5c4cfb170f5081176
fionnasf <34213826+fionnasf@users.noreply.github.com>
2024-01-15 20:14:32 +0700

    Automate

---
+++ Register - positive.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Register test with positive test case</description>
+   <name>Register - positive</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a898a2cf-9db0-49d3-96b1-d642a731c717</testSuiteGuid>
+   <testCaseLink>
+      <guid>159aed39-ff69-4e3e-ba23-fbb7881ad05a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Register Positive</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
